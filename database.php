9566b39b8160521b0e2c3422f9107cac7e239e30
villanuevajennilea <villanuevajennilea@gmail.com>
2022-09-09 22:24:47 +0800

    database added

---
+++ database.php
@@ -0,0 +1,23 @@
+<?php
+        $server_name = "localhost";
+        $db_username = "root";
+        $db_password = "";
+        $db_name = "juncle";
+
+        //Connections: You can use '$connection' or '$conn' the later is shorter.
+        $connection = mysqli_connect($server_name, $db_username, $db_password);
+        $conn = mysqli_connect($server_name, $db_username, $db_password);
+        
+        $dbconfig = mysqli_select_db($connection,$db_name);
+
+        if($dbconfig){
+            // echo 'Connected to Database';
+            ?>
+
+            <!-- <h1>SDS</h1> -->
+<?php
+        }else{
+            // echo 'Unable to connect to database;';
+        }
+
+?>
